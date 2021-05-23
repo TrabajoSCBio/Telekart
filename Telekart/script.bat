403888e4bd895c5c6cfbc15d2b0d9cbf78c8f846
BryanVN1999 <jhovarne@teleco.upv.es>
2021-05-23 19:19:37 +0200

    Juego del año

--- Telekart/script.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-CLS
-cd yolov5
-.\venv\Scripts\python.exe launcher.py
-CLS
--- Telekart/yolov5/setup.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup,Executable
-setup(
-    name="RedNeuronal",
-    version=0.1,
-    description="project_description",
-    executables=[Executable("launcher.py",base="Win32GUI")],
-)
